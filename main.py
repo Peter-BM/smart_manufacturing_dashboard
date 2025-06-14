8db5333bb75fb2a6e63a44cc8aa41f6e23365f31
Koko <pedrobobella@gmail.com>
2025-06-14 17:51:30 -0300

    feat: Add a vibration graph to the dashboard.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 PATH = "data/smart_manufacturing_data_cleaned.csv"
 
@@ -14,6 +15,29 @@ def read_data():
 def create_date_feature(df):
     df["timestamp"] = pd.to_datetime(df["timestamp"])
     df["date"] = df["timestamp"].dt.date
+
+
+def format_quarter(quarter):
+    quarters = {"Madrugada": 0, "Manhã": 1, "Tarde": 2, "Noite": 3}
+
+    return quarters[quarter]
+
+
+def display_machine_vibration_graph(df, query, selected_machine):
+    filtered_df = df.query(query)
+
+    if filtered_df.empty:
+        st.warning("Nenhum dado encontrado para os filtros selecionados.")
+        return
+
+    fig = px.line(
+        filtered_df,
+        x="timestamp",
+        y="vibration",
+        title=f"Vibração da máquina {selected_machine}",
+        labels={"vibration": "Vibração", "timestamp": "Data e Hora"},
+    )
+    st.plotly_chart(fig, use_container_width=True)
 
 
 if __name__ == "__main__":
@@ -33,10 +57,6 @@ if __name__ == "__main__":
         max_date = df["date"].max()
         quarters = ["Madrugada", "Manhã", "Tarde", "Noite"]
 
-        selected_machine = st.selectbox(
-            "Escolha uma máquina", options=df["machine"].unique()
-        )
-
         with col1:
             start_date = st.slider(
                 "Data inicial",
@@ -46,6 +66,7 @@ if __name__ == "__main__":
                 format="DD/MM/YYYY",
             )
             start_quarter = st.selectbox("Periodo inicial", options=quarters, index=0)
+            start_quarter = format_quarter(start_quarter)
 
         with col2:
             end_date = st.slider(
@@ -56,5 +77,16 @@ if __name__ == "__main__":
                 format="DD/MM/YYYY",
             )
             end_quarter = st.selectbox("Periodo final", options=quarters, index=2)
+            end_quarter = format_quarter(end_quarter)
+
+        selected_machine = st.selectbox("Máquina", options=df["machine"].unique())
 
         submitted = st.form_submit_button("Análisar", type="primary")
+
+    filter = """
+    (@start_date <= date and date <= @end_date) and \
+    (@start_quarter <= slot_6h <= @end_quarter) and \
+    (machine == @selected_machine)
+    """
+
+    display_machine_vibration_graph(df, filter, selected_machine)
